952f475935c7412363ad34f5130d905524901c2d
Rossil2012 <40714231+Rossil2012@users.noreply.github.com>
2020-08-10 12:41:04 +0800

    dev: add code_to_bytes parser By rossil2012

--- pyMapReduce/Job/__init__.py
+++ pyMapReduce/Job/__init__.py
@@ -1,5 +1,25 @@
+import inspect
 from Exception import UndefException
 from abc import ABCMeta, abstractmethod
+
+
+def parse_code_to_bytes(source: str):
+    parsed_code = str()
+    lines = source.splitlines(False)
+
+    def_line = lines[0].replace('(self)', '()', 1)
+    stripped_def_line = def_line.lstrip()
+    lines.pop(0)
+    indent = len(def_line) - len(stripped_def_line)
+    parsed_code += stripped_def_line + '\n'
+
+    for line in lines:
+        stripped_line = line.rstrip()
+        if len(stripped_line) != 0:
+            parsed_code += stripped_line[indent:] + '\n'
+
+    code_bytes = compile(parsed_code, '<string>', 'exec')
+    return code_bytes.co_consts[0]
 
 
 class Job(metaclass=ABCMeta):
@@ -16,4 +36,5 @@ class Job(metaclass=ABCMeta):
         raise UndefException('Reduce')
 
     def run(self):
+        code_bytes = parse_code_to_bytes(inspect.getsource(self.map))
         pass
