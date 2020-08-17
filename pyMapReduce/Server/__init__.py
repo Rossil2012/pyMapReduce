23a6739f7f66fac75a5e85fafa3ae323d904637f
Rossil2012 <40714231+Rossil2012@users.noreply.github.com>
2020-08-17 13:25:36 +0800

    dev: finish basic functions By rossil2012

--- pyMapReduce/Server/__init__.py
+++ pyMapReduce/Server/__init__.py
@@ -4,33 +4,35 @@ import socketserver
 import hashlib
 import inspect
 import sys
+import math
+import time
+import json
+from queue import Queue
 from enum import Enum
 from types import FunctionType
-from multiprocessing import Lock, Process
-
-__all__ = [
-    'Master',
-    'Slave'
-]
+from multiprocessing import Lock, Process, Manager
 
 
 class _MsgType(Enum):
 
-    Heartbeat = 0
-    Heartbeat_Res = 1
+    HeartBeat = 0
+    HeartBeat_Res = 1
 
-    MapTask = 2
-    MapTask_Res = 3
+    AllTask = 2
+    AllTask_Res = 3
 
-    ReduceTask = 4
-    ReduceTask_Res = 5
+    MapTask = 4
+    MapTask_Res = 5
+
+    ReduceTask = 6
+    ReduceTask_Res = 7
 
 
 def _parse_code_to_func(source: str) -> FunctionType:
     parsed_code = str()
     lines = source.splitlines(False)
 
-    def_line = lines[0].replace('(self)', '()', 1)
+    def_line = lines[0].replace('(self', '(', 1)
     stripped_def_line = def_line.lstrip()
     lines.pop(0)
     indent = len(def_line) - len(stripped_def_line)
@@ -46,11 +48,21 @@ def _parse_code_to_func(source: str) -> FunctionType:
     return FunctionType(code_bytes.co_consts[0], {'__builtins__': __builtins__})
 
 
+def _connect(ip: str, port: int):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    sock.connect((ip, port))
+
+    return sock
+
+
 def _recv_exact_n_bytes(sock: socket.socket, n: int):
+    fetch_max = 1024
     cur_bytes = 0
     ret = bytes()
     while cur_bytes < n:
-        fetch = sock.recv(n - cur_bytes)
+        to_fetch = fetch_max if n - cur_bytes > fetch_max else n - cur_bytes
+        fetch = sock.recv(to_fetch)
         ret += fetch
         cur_bytes += len(fetch)
 
@@ -58,41 +70,61 @@ def _recv_exact_n_bytes(sock: socket.socket, n: int):
 
 
 def _receive_and_decode_Msg(sock: socket.socket):
-    try:
-        msg_type = _MsgType(int.from_bytes(_recv_exact_n_bytes(sock, 1), 'big'))
+    def _get_body_content_one():
+        body_bytes = int.from_bytes(_recv_exact_n_bytes(sock, 8), 'big')
+        return bytes.decode(_recv_exact_n_bytes(sock, body_bytes), 'utf-8')
 
-        fingerprint = None
+    msg_type = _MsgType(int.from_bytes(_recv_exact_n_bytes(sock, 1), 'big'))
+    blocking_state = sock.getblocking()
+    sock.setblocking(True)
 
-        if msg_type == _MsgType.Heartbeat or msg_type == _MsgType.Heartbeat_Res:
-            body_bytes = int.from_bytes(_recv_exact_n_bytes(sock, 8), 'big')
-            body = {
-                'slave_id': bytes.decode(_recv_exact_n_bytes(sock, body_bytes), 'utf-8')
-            }
-        else:
-            fingerprint = hex(int.from_bytes(sock.recv(32), 'big'))[2:]
-            body = dict()
+    fingerprint = None
 
-            if msg_type == _MsgType.MapTask:
-                body_bytes = int.from_bytes(_recv_exact_n_bytes(sock, 8), 'big')
-                body['MapFunc'] = bytes.decode(_recv_exact_n_bytes(sock, body_bytes), 'utf-8')
+    if msg_type == _MsgType.HeartBeat or msg_type == _MsgType.HeartBeat_Res:
+        body = {
+            'slave_id': _get_body_content_one()
+        }
+    else:
+        fingerprint = hex(int.from_bytes(sock.recv(32), 'big'))[2:]
+        body = dict()
 
-                body_bytes = int.from_bytes(_recv_exact_n_bytes(sock, 8), 'big')
-                body['FileFunc'] = bytes.decode(_recv_exact_n_bytes(sock, body_bytes), 'utf-8')
+        if msg_type == _MsgType.AllTask:
+            body['mapFunc'] = _get_body_content_one()
+            body['reduceFunc'] = _get_body_content_one()
+            body['file'] = json.loads(_get_body_content_one())
 
-        return msg_type, fingerprint, body
+        elif msg_type == _MsgType.MapTask:
+            body['mapFunc'] = _get_body_content_one()
+            body['file'] = json.loads(_get_body_content_one())
 
-    except Exception as e:
-        print(e)
-        return None, None, None
+        elif msg_type == _MsgType.ReduceTask:
+            body['reduceFunc'] = _get_body_content_one()
+            body['file'] = json.loads(_get_body_content_one())
+
+        elif msg_type == _MsgType.MapTask_Res or msg_type == _MsgType.ReduceTask_Res:
+            body['result'] = json.loads(_get_body_content_one())
+
+    sock.setblocking(blocking_state)
+
+    return msg_type, fingerprint, body
 
 
-def _connect(ip: str, port: int, req: bytes = bytes()):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((ip, port))
-    if len(req):
-        sock.sendall(req)
+def _get_all_Msgs(sock: socket.socket):
+    all_msgs = []
+    blocking_state = sock.getblocking()
+    sock.setblocking(False)
 
-    return sock
+    while True:
+        try:
+            msg_type, fingerprint, body = _receive_and_decode_Msg(sock)
+            all_msgs.append({
+                'msg_type': msg_type,
+                'fingerprint': fingerprint,
+                'body': body
+            })
+        except BlockingIOError:
+            sock.setblocking(blocking_state)
+            return all_msgs
 
 
 def _make_req(msg_type: _MsgType, body: list, fingerprint: str = str()):
@@ -119,153 +151,222 @@ class _ThreadedMasterTCPRequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
         msg_type, fingerprint, body = _receive_and_decode_Msg(self.request)
 
-        if msg_type == _MsgType.MapTask:
-            prepared_slaves = []
+        if msg_type == _MsgType.AllTask:
+            if fingerprint in self.server._tasks:
+                # !!!!!!
+                print('Task', fingerprint, 'is running...')
+                return
 
-            self.server.task_lock.acquire()
-            self.server.task_dict[fingerprint] = body
-            self.server.task_lock.release()
+            distributed_tasks = self.server._distribute_tasks(body['file'])
+            self.server._tasks_lock.acquire()
+            self.server._tasks[fingerprint] = {
+                'map': {
+                    'func': body['mapFunc'],
+                    'file': json.dumps(body['file']),
+                    'running_sub_tasks': distributed_tasks,
+                    'result': []
+                },
+                'reduce': {
+                    'func': body['reduceFunc'],
+                    'running_sub_tasks': dict(),
+                    'result': []
+                }
+            }
+            self.server._tasks_lock.release()
 
-            for s_id, s in self.server.slave_dict.items():
-                map_req = _make_req(_MsgType.MapTask, [body['MapFunc'], body['FileFunc']], fingerprint)
-                sock = _connect(s['ip'], s['port'], map_req)
-                _msg_type, _fingerprint, _body = _receive_and_decode_Msg(sock)
-                if _msg_type == _MsgType.MapTask_Res and _fingerprint == fingerprint:
-                    prepared_slaves.append(s_id)
-                    print(s_id, 'is prepared for Map task')
-                else:
-                    print('Cannot connect to', s['slave_id'], 'when assigning Map task')
-
-            self.request.sendall(_make_req(_MsgType.MapTask_Res, prepared_slaves, fingerprint))
-
-
-class _ThreadedSlaveTCPRequestHandler(socketserver.BaseRequestHandler):
-
-    def handle(self):
-        msg_type, fingerprint, body = _receive_and_decode_Msg(self.request)
-
-        if msg_type == _MsgType.Heartbeat:
-            self.request.sendall(_make_req(_MsgType.Heartbeat_Res, body.values()))
-
-        elif msg_type == _MsgType.MapTask:
-            self.server.task_lock.acquire()
-            self.server.task_dict[fingerprint] = body
-            self.server.task_lock.release()
-
-            self.request.sendall(_make_req(_MsgType.MapTask_Res, [], fingerprint))
-            # !!!!!!!!
-            MapFunc = _parse_code_to_func(body['MapFunc'])
-            FileFunc = _parse_code_to_func(body['FileFunc'])
-            MapFunc()
-            FileFunc()
+            for s_id, file in distributed_tasks.items():
+                map_req = _make_req(_MsgType.MapTask, [body['mapFunc'], json.dumps(file)], fingerprint)
+                self.server._safe_sendall(self.server._slaves[s_id]['conn'], map_req)
 
 
 class _ThreadedMasterTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-    task_lock = Lock()
-    task_dict = dict()
-
-    slave_lock = Lock()
-    slave_dict = dict()
+    pass
 
 
-class _ThreadedSlaveTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-    task_lock = Lock()
-    task_dict = dict()
-
-
-class _Server:
-
-    def __init__(self, server):
-        self._server = server
-        self.__server_running = False
-
-    def run(self):
-        if self.__server_running:
-            print('Master server has been started.')
-        else:
-            server_thread = threading.Thread(target=self._server.serve_forever)
-            server_thread.daemon = True
-            server_thread.start()
-            self.__server_running = True
-            # print("Server loop running in thread:", server_thread.name)
-
-    def close(self):
-        if not self.__server_running:
-            print('Master server has not been started.')
-        else:
-            self._server.shutdown()
-            self._server.server_close()
-            self.__server_running = False
-
-    def server_address(self):
-        return self._server.server_address
-
-
-class Master(_Server):
+class Master(_ThreadedMasterTCPServer):
 
     def __init__(self):
-        _Server.__init__(self, _ThreadedMasterTCPServer(('', 0), _ThreadedMasterTCPRequestHandler))
+        _ThreadedMasterTCPServer.__init__(self, ('', 0), _ThreadedMasterTCPRequestHandler)
+        self._socket_queue = Queue()
+        self._server_running = False
+        self._slaves = dict()
+        self._tasks = dict()
+        self._tasks_lock = Lock()
+
+    def _safe_sendall(self, sock: str, req: bytes):
+        self._socket_queue.put({
+            sock: req
+        })
+
+    def _distribute_tasks(self, file: list):
+        print(file)
+        ret = dict()
+        alive_slaves = []
+        for s_id, s_item in self._slaves.items():
+            if s_item['alive']:
+                alive_slaves.append(s_id)
+
+        len_each = math.floor(len(file) / len(alive_slaves))
+        for i in range(0, len(alive_slaves) - 1):
+            ret[alive_slaves[i]] = file[i*len_each:(i+1)*len_each]
+
+        ret[alive_slaves[len(alive_slaves)-1]] = file[(len(alive_slaves)-1)*len_each:]
+        print(ret)
+
+        return ret
+
+    def _threaded_sendall(self):
+        while True:
+            task = self._socket_queue.get()
+            for sock, req in task.items():
+                sock.sendall(req)
+
+    def _handle_Msgs(self, slave_id, msgs):
+        print(msgs)
+        self._slaves[slave_id]['timeout_cnt'] += 1
+        self._safe_sendall(self._slaves[slave_id]['conn'], _make_req(_MsgType.HeartBeat, [slave_id]))
+
+        for msg in msgs:
+            if msg['msg_type'] == _MsgType.HeartBeat_Res:
+                self._slaves[slave_id]['timeout_cnt'] = 0
+                self._slaves[slave_id]['alive'] = True
+
+            elif msg['msg_type'] == _MsgType.MapTask_Res:
+                map_task_rec = self._tasks[msg['fingerprint']]['map']
+                map_task_rec['running_sub_tasks'].pop(slave_id)
+                map_task_rec['result'].extend(msg['body']['result'])
+
+                if len(map_task_rec['running_sub_tasks']) == 0:
+                    print('all', self._tasks[msg['fingerprint']])
+                    print('maptaskrec', map_task_rec)
+                    reduce_task_rec = self._tasks[msg['fingerprint']]['reduce']
+                    reduce_task_rec['running_sub_tasks'] = self._distribute_tasks(map_task_rec['result'])
+
+                    for s_id, file in reduce_task_rec['running_sub_tasks'].items():
+                        reduce_req = _make_req(_MsgType.ReduceTask, [reduce_task_rec['func'], json.dumps(file)], msg['fingerprint'])
+                        self._safe_sendall(self._slaves[s_id]['conn'], reduce_req)
+
+            elif msg['msg_type'] == _MsgType.ReduceTask_Res:
+                reduce_task_rec = self._tasks[msg['fingerprint']]['reduce']
+                reduce_task_rec['running_sub_tasks'].pop(slave_id)
+                reduce_task_rec['result'].extend(msg['body']['result'])
+                print(msg['body']['result'])
+
+                if len(reduce_task_rec['running_sub_tasks']) == 0:
+                    self._tasks.pop(msg['fingerprint'])
+                    print('Final Result:', reduce_task_rec['result'])
+
+        if self._slaves[slave_id]['timeout_cnt'] > 2:
+            self._slaves[slave_id]['alive'] = False
+            print(slave_id, 'is down.')
 
     def register_slave(self, slave_id: str, slave_ip: str, slave_port: int):
-        if slave_id in self._server.slave_dict:
+        if slave_id in self._slaves:
             print('Slave', '"' + slave_id + '"', 'already exists')
         else:
-            req = _make_req(_MsgType.Heartbeat, [slave_id])
-            conn = _connect(slave_ip, slave_port, req)
-            msg_type, fingerprint, body = _receive_and_decode_Msg(conn)
-            conn.close()
+            conn = _connect(slave_ip, slave_port)
+            self._slaves[slave_id] = {
+                'conn': conn,
+                'alive': True,
+                'timeout_cnt': 0
+            }
+            self._safe_sendall(self._slaves[slave_id]['conn'], _make_req(_MsgType.HeartBeat, [slave_id]))
 
-            if msg_type == _MsgType.Heartbeat_Res and body['slave_id'] == slave_id:
-                print('Successfully register slave', '"' + slave_id + '"')
-                self._server.slave_dict[slave_id] = {
-                    'ip': slave_ip,
-                    'port': slave_port
-                }
+            print('Successfully register slave', '"' + slave_id + '"')
+
+    def run(self):
+        if self._server_running:
+            print('Master server has been started.')
+        else:
+            server_thread = threading.Thread(target=self.serve_forever)
+            server_thread.daemon = True
+            server_thread.start()
+            self._server_running = True
+
+            socket_send_thread = threading.Thread(target=self._threaded_sendall)
+            socket_send_thread.daemon = True
+            socket_send_thread.start()
+
+            print('Listen on', self.server_address)
+
+            while True:
+                for slave_id, s in self._slaves.items():
+                    all_msgs = _get_all_Msgs(s['conn'])
+                    self._handle_Msgs(slave_id, all_msgs)
+
+                time.sleep(5)
+
+    def close(self):
+        if not self._server_running:
+            print('Master server has not been started.')
+        else:
+            self.shutdown()
+            self.server_close()
+            self._server_running = False
 
 
-class Slave(_Server):
+class Slave:
 
     def __init__(self):
-        _Server.__init__(self, _ThreadedSlaveTCPServer(('', 0), _ThreadedSlaveTCPRequestHandler))
+        self._tasks = dict()
+        self._jobs = dict()
+        self._manager = Manager()
+        self._job_results = self._manager.dict()
+        self._conn = socket.socket()
 
-# master = Master()
-# slave = Slave()
-# master.run()
-# slave.run()
-# ip, port = slave.server_address()
-# master.register_slave('a', ip, port)
-# master.register_slave('a', ip, port)
-# ip, port = master.server_address()
-# print(type(ip), type(port))
-# _client(ip, port, '123')
-# client(ip, port, '123')
-# client(ip, port, '123')
-# master.close()
+    def _handle_Msg(self, msg_type, fingerprint, body):
+        def handle_MapReduceTask():
+            func = _parse_code_to_func(body['mapFunc']) if msg_type == _MsgType.MapTask else _parse_code_to_func(body['reduceFunc'])
 
-master = Master()
-slave1 = Slave()
-slave2 = Slave()
-master.run()
-slave1.run()
-slave2.run()
-ip, port = slave1.server_address()
-master.register_slave('slave1', ip, port)
-ip, port = slave2.server_address()
-master.register_slave('slave2', ip, port)
+            res = []
+            for pair in body['file']:
+                for k, v in pair.items():
+                    res.extend(func(k, v))
 
+            self._job_results[fingerprint] = {
+                'result': res,
+                'msg_type': msg_type
+            }
 
-def MapFunc():
-    print('Map Func')
+        if msg_type == _MsgType.HeartBeat:
+            self._conn.sendall(_make_req(_MsgType.HeartBeat_Res, [body['slave_id']]))
 
+        elif msg_type == _MsgType.MapTask or msg_type == _MsgType.ReduceTask:
+            map_process = Process(target=handle_MapReduceTask)
+            map_process.start()
+            self._jobs[fingerprint] = map_process
 
-def FileFunc():
-    print('File Func')
+    def _handle(self):
+        while True:
+            try:
+                msg_type, fingerprint, body = _receive_and_decode_Msg(self._conn)
+            except BlockingIOError:
+                time.sleep(3)
+                continue
+            print(msg_type, fingerprint, body)
+            self._handle_Msg(msg_type, fingerprint, body)
 
+            finished_tasks = []
+            for _fingerprint, job in self._jobs.items():
+                if not job.is_alive():
+                    finished_tasks.append(_fingerprint)
+                    print(self._job_results[_fingerprint])
+                    res = self._job_results[_fingerprint]
+                    _msg_type = _MsgType.MapTask_Res if res['msg_type'] == _MsgType.MapTask else _MsgType.ReduceTask_Res
+                    self._conn.sendall(_make_req(_msg_type, [json.dumps(res['result'])], _fingerprint))
 
-fingerprint = hashlib.sha256((ip + str(port) + inspect.getsource(MapFunc) + inspect.getsource(FileFunc)).encode('utf-8')).hexdigest()
-map_req = _make_req(_MsgType.MapTask, [inspect.getsource(MapFunc), inspect.getsource(FileFunc)], fingerprint)
-ip, port = master.server_address()
-print(ip, port)
-_connect(ip, port, map_req)
-while True:
-    pass
+            for task in finished_tasks:
+                self._jobs.pop(task)
+                self._job_results.pop(task)
+
+    def run(self):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(('', 0))
+        server.listen(1)
+        print('Listen on', server.getsockname())
+        conn, address = server.accept()
+        conn.setblocking(False)
+
+        self._conn = conn
+        self._handle()
+
